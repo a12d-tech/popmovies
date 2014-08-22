ddb5869e14c810074b8af68b395b931ee2a95cbc
gntics <alexandre.quard@gmail.com>
2014-08-22 16:18:10 +0200

    remove unused test from popmovies

--- spec/popmovies_spec.rb
+++ spec/popmovies_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe Popmovies do
   it 'has a version number' do
-    expect(Popmovies::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Popmovies::VERSION).not_to be_nil
   end
 end
