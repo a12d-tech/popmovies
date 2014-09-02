226f4be036d4968131f83b1610f3744d6eb5adb3
gntics <alexandre.quard@gmail.com>
2014-09-03 01:52:24 +0200

    remove comments in application.rb

--- lib/popmovies/application.rb
+++ lib/popmovies/application.rb
@@ -23,14 +23,5 @@ module Popmovies
       Kernel.abort
     end
 
-      # videos_list_tmp = page.css("div.filmborder div.filmcontent div.filmicerik")
-      # videos_list = videos_list_tmp.xpath("./p//iframe[contains(@src,'embed')]")
-
-      # episodes_list_tmp = page.css("div.filmborder div.filmcontent div.keremiya_part")
-      # episodes_list = episodes_list_tmp.xpath('./a')
-
-      # seasons_list = page.css("div.filmborder div.filmcontent div.moviefilm div.movief a")
-      # series_list = page.css("div.sidebarborder div.sidebar-right ul li.cat-item a")
-
   end
 end
