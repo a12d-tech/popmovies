6eaddcc773f05ccddaffe74909c7e826112684d6
gntics <alexandre.quard@gmail.com>
2014-09-30 19:14:46 +0200

    refactor seasons controller

--- lib/popmovies/controllers/seasons_controller.rb
+++ lib/popmovies/controllers/seasons_controller.rb
@@ -4,37 +4,30 @@ require_relative '../models/season'
 
 module Popmovies
   module Controllers
-    class SeasonsController
+    class SeasonsController < Controller
       include Views
       include Models
 
       SEASONS_CSS_SELECTOR = "div.filmborder div.filmcontent div.moviefilm div.movief a"
 
       def initialize router
+        super()
         @season_view = SeasonView.new router
       end
 
       def index tv_show
-        @seasons = fetch_datas tv_show
+        @seasons = fetch_datas(tv_show.url,SEASONS_CSS_SELECTOR){
+          seasons = []
+          @html_selected_tags.each do |season|
+            season_title = season.text
+            season_url = season['href']
+            seasons << Season.new(season_title, season_url)
+            tv_show.seasons << seasons
+          end
+         @datas = seasons
+        }
         @season_view.update @seasons
         @season_view.render
-      end
-
-      def fetch_datas tv_show
-        # TODO : test it !!
-        # check the resule of fetch_html_page and page.css
-        # depends on Exception thrown by nokogiri and open-uri on Utils class
-        page = Utils.fetch_html_page tv_show.url
-        html_seasons = page.css SEASONS_CSS_SELECTOR
-
-        seasons = []
-        html_seasons.each do |season|
-          season_title = season.text
-          season_url = season['href']
-          seasons << Season.new(season_title, season_url)
-          tv_show.seasons << seasons
-        end
-        return seasons
       end
 
     end
