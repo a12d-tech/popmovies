e37af952b6de29db061df2078075b847a0524eb8
gntics <alexandre.quard@gmail.com>
2014-09-30 08:46:15 +0200

    add small test to popmovies spec

--- spec/popmovies_spec.rb
+++ spec/popmovies_spec.rb
@@ -4,4 +4,9 @@ describe Popmovies do
   it 'has a version number' do
     expect(Popmovies::VERSION).not_to be_nil
   end
+  describe "#start" do
+    it "method present?" do
+      expect(Popmovies).to respond_to(:start)
+    end
+  end
 end
