6ec8c6581ebb01cdf352e4a56f1d91ac126f8252
gntics <alexandre.quard@gmail.com>
2014-10-01 17:00:55 +0200

    refactor links controller

--- lib/popmovies/controllers/links_controller.rb
+++ lib/popmovies/controllers/links_controller.rb
@@ -4,36 +4,28 @@ require_relative '../models/link'
 
 module Popmovies
   module Controllers
-    class LinksController
+    class LinksController < Controller
       include Views
       include Models
 
-      LINKS_CSS_SELECTOR = "div.filmborder div.filmcontent div.filmicerik"
-
       def initialize router
+        super()
         @link_view = LinkView.new router
       end
 
       def index episode
-        @links = fetch_datas episode
+        @links = fetch_datas(episode.url, Config::WEB[:links_css_selector]){
+          # filter links
+          @html_selected_tags = @html_selected_tags.xpath("./p//iframe[contains(@src,'embed')]")
+
+          links = []
+          @html_selected_tags.each { |link_url| links << Link.new(link_url['src']) }
+          @datas = links
+        }
         @link_view.update @links
         @link_view.render
       end
 
-      def fetch_datas episode
-        # TODO : test it !!
-        # check the resule of fetch_html_page and page.css
-        # depends on Exception thrown by nokogiri and open-uri on Utils class
-        page = Utils.fetch_html_page episode.url
-        html_links_tmp = page.css LINKS_CSS_SELECTOR
-        html_links = html_links_tmp.xpath("./p//iframe[contains(@src,'embed')]")
-
-        links = []
-        html_links.each do |link_url|
-          links << Link.new(link_url['src'])
-        end
-        return links
-      end
     end
   end
 end
