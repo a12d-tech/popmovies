7c766c676874ee863585eb910663ca6939e639b2
gntics <alexandre.quard@gmail.com>
2014-10-02 18:33:02 +0200

    refactor episode ctrler & css selector

--- lib/popmovies/config.rb
+++ lib/popmovies/config.rb
@@ -1,10 +1,10 @@
 module Popmovies
   module Config
-    WEB = { 
+    WEB = {
       :endpoint => "http://streaming-series.org/",
       :tv_shows_css_selector => "div.sidebarborder div.sidebar-right ul li.cat-item a",
       :seasons_css_selector => "div.filmborder div.filmcontent div.moviefilm div.movief a",
-      :episodes_css_selector => "div.filmborder div.filmcontent div.keremiya_part",
+      :episodes_css_selector => "div.filmborder div.filmcontent div.keremiya_part a",
       :links_css_selector => "div.filmborder div.filmcontent div.filmicerik"
     }
   end
--- lib/popmovies/controllers/episodes_controller.rb
+++ lib/popmovies/controllers/episodes_controller.rb
@@ -14,14 +14,7 @@ module Popmovies
 
       def index season
         @episodes = fetch_datas(season.url, Config::WEB[:episodes_css_selector]) do |html_tags|
-          # specific tmp !! to remove
-          html_tags = html_tags.xpath('./a')
-
           tmp = html_tags.map { |episode| Episode.new(episode.css("span").text, episode['href']) }
-          # TODO add episodes to tv_show.episode
-          # tv_show.seasons << seasons
-
-          # ugly but no choice
           [Episode.new("Episode 1", season.url), tmp].flatten
         end
         @episode_view.update @episodes
