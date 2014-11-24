17a22d70dc70a7cd2a91e07818d552417b4a3f19
gntics <alexandre.quard@gmail.com>
2014-11-24 15:53:37 +0100

    fixed episode selector

--- lib/popmovies/config.rb
+++ lib/popmovies/config.rb
@@ -4,7 +4,7 @@ module Popmovies
       :endpoint => "http://streaming-series.org/",
       :tv_shows_css_selector => "div.sidebarborder div.sidebar-right ul li.cat-item a",
       :seasons_css_selector => "div.filmborder div.filmcontent div.moviefilm div.movief a",
-      :episodes_css_selector => "div.filmborder div.filmcontent div.keremiya_part a",
+      :episodes_css_selector => "div.filmborder div.filmcontent div.keremiya_part > a",
       :links_css_selector => "div.filmborder div.filmcontent div.filmicerik"
     }
   end
